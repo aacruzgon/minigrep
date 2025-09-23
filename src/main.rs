2a03ba89dc770089425c869626f02f96745ab6ee
acruzgon <acruzgon@uwo.ca>
2025-09-23 14:05:06 -0700

    improved Cofig code with closures and search function to use iterators logic.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use std::fs;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -41,24 +39,46 @@ pub struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let (_lower, upper) = args.size_hint();
 
-        let mut ignore_case = env::var("IGNORE_CASE").is_ok();
-
-        // ci = case insentive
-        if args.len() == 4 {
-            match args[3].as_str() {
-                "--ignore-case" => ignore_case = true,
-                "--case-sensitive" => ignore_case = false,
-                _ => return Err("Invalid argument, check manual for valid arguments"),
+        if let Some(ub) = upper {
+            if ub < 2 {
+                return Err(
+                    "not enough arguments: usage: <query> <file_path> [--ignore-case|--case-sensitive]",
+                );
             }
         }
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let mut ignore_case = match env::var("IGNORE_CASE") {
+            Ok(v) if matches!(v.to_lowercase().as_str(), "1" | "true") => true,
+            _ => false,
+        };
+
+        if let Some(flag) = args.next() {
+            match flag.as_str() {
+                "--ignore-case" => ignore_case = true,
+                "--case-sensitive" => ignore_case = false,
+                _ => {
+                    return Err("invalid argument: expected --ignore-case or --case-sensitive");
+                }
+            }
+        }
+
+        if args.next().is_some() {
+            return Err("too many arguments");
+        }
 
         Ok(Config {
             query,
